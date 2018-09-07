af458d32f9b88de8650991d80d8cbd9c770da4f9
michal.hvila <michal.hvila@gmail.com>
2018-09-07 16:29:31 +0200

    MHv

--- TEMP.py
+++ TEMP.py
@@ -1,16 +1,32 @@
 
-print("------------ 16.2.2 ------------")
-print('===== GRAFICKE OBJEKTY =====')
+# DEDENIE Z NDRADENEJ TRIEDY UTVAR
 
 from tkinter import *
+import random
 
-class Kruh:
+class Utvar:
     canvas = None
-    typ = 'kruh'
+
+    def __init__(self, x, y, farba='red' ):
+        self.x, self.y = x, y
+        self.farba = farba
+        self.id = None
+
+    def posun(self, dx=0, dy=0):
+        self.x += dx
+        self.y += dy
+        self.canvas.move(self.id, dx, dy)
+
+    def prefarbi(self, farba):
+        self.farba = farba
+        self.canvas.itemconfig(self.id, fill=self.farba)
+
+class Kruh(Utvar):
+    canvas = None
 
     def __init__(self, x, y, r, farba='red'):
-        self.x, self.y, self.r = x, y, r
-        self.farba = farba
+        super().__init__(x, y, farba)
+        self.r = r
         self.id = canvas.create_oval(self.x+self.r, self.y+self.r,
                                      self.x-self.r, self.y-self.r,
                                      fill = self.farba)
@@ -19,29 +35,18 @@ class Kruh:
                                               self.x-self.r, self.y-self.r,
                                               repr(self.farba))
 
-    def posun(self, dx=0, dy=0):
-        self.x += dx
-        self.y += dy
-        self.canvas.move(self.id, dx, dy)
-
     def zmen(self, r):
         self.r = r
         self.canvas.coords(self.id,
                            self.x + self.r, self.y + self.r,
                            self.x - self.r, self.y - self.r)
 
-    def prefarbi(self, farba):
-        self.farba = farba
-        self.canvas.itemconfig(self.id, fill=self.farba)
-
-
-class Obdlznik:
+class Obdlznik(Utvar):
     canvas = None
-    typ = 'obdlznik'
 
     def __init__(self, x, y, sirka, vyska, farba='red'):
-        self.x, self.y, self.sirka, self.vyska = x, y, sirka, vyska
-        self.farba = farba
+        super().__init__(x, y, farba)
+        self.sirka, self.vyska = sirka, vyska
         self.id = canvas.create_rectangle(self.x, self.y,
                                           self.x+sirka, self.y+vyska,
                                           fill=self.farba)
@@ -50,20 +55,11 @@ class Obdlznik:
         return 'Obdlznik({},{},{},{},{})' .format(self.x, self.y, self.sirka, self.vyska, self.farba)
 
 
-    def posun(self, dx=0, dy=0):
-        self.x += dx
-        self.y += dy
-        self.canvas.move(self.id, dx, dy)
-
     def zmen(self, sirka, vyska):
         self.r = r
         self.canvas.coords(self.id,
                            self.x, self.y,
                            self.x+sirka, self.y+vyska)
-
-    def prefarbi(self, farba):
-        self.farba = farba
-        self.canvas.itemconfig(self.id, fill=self.farba)
 
 class Skupina:
     def __init__(self):
@@ -83,12 +79,12 @@ class Skupina:
 
     def prefarbi_typ(self, typ, farba):
         for utvar in self.pole:
-            if utvar.typ == typ:
+            if isinstance(utvar, typ):
                 utvar.prefarbi(farba)
 
     def posun_typ(self, typ, dx, dy):
         for utvar in self.pole:
-            if utvar.typ == typ:
+            if isinstance(utvar, typ):
                 utvar.posun(dx, dy)
 
     def citaj(self):
@@ -109,15 +105,32 @@ c = Kruh.canvas = Obdlznik.canvas = canvas
 c.pack()
 
 skupina_utvarov = Skupina()
-k = Kruh(50, 50, 30, 'blue')
-r = Obdlznik(100, 20, 100, 50)
+t1 = Kruh(50, 50, 30)
+t2 = Obdlznik(100, 20, 100, 50)
 
-skupina_utvarov.pridaj(k)
-skupina_utvarov.pridaj(r)
-#skupina_utvarov.citaj()
+# isinstance(i, t), ktorá zistí, či je inštancia i typu t alebo je typom niektorého jeho predka
+print(isinstance(t1, Utvar))        # True
+print(isinstance(t1, Kruh))         # True
+print(isinstance(t1, Obdlznik))     # False
+print(isinstance(t2, Obdlznik))     # True
+# print(type(t1))     # <class '__main__.Kruh'>
+# print(type(t2))     # <class '__main__.Obdlznik'>
+
+t1.prefarbi('green')
+t2.posun(50)
+
+skupina_utvarov.pridaj(t1)
+skupina_utvarov.pridaj(t2)
 print('Obsah skupiny: [{}]' .format(', '.join(skupina_utvarov.citaj())))
 
-k.prefarbi('green')
-r.posun(50)
+skupina_utvarov.prefarbi_typ(Kruh, 'black')
+skupina_utvarov.posun_typ(Obdlznik, -10, -25)
+
+
+
+
+
 
 root.mainloop()
+
+
